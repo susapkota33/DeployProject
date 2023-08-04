24cf4cb26bf5cda50a935a5c401963541804797d
Trainee3 <Trainee3@gmail.com>
2023-08-04 10:13:20 -0400

    chaged on app.js

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./Layout";
+import Layout from '/.Layout';
 import Home from "./Home";
 import About from "./About";
 import Contact from "./Contact";
